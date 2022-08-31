519a9e5c082ae7b26769fde1b711307f46671f13
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-08-31 14:04:53 -0300

    Doc: install scss

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
-function App() {
+function Home() {
   const [count, setCount] = useState(0)
 
   return (
@@ -31,4 +30,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
