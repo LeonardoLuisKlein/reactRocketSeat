e8e4edd65a72cb742f7bb182789f8c5b973afa4f
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-08-31 15:38:52 -0300

    Feat: input logic and style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,51 @@
-import { useState } from 'react'
-import {Card} from '../../components/card/index'
-import './style.scss'
+import { useState } from "react";
+import { Card } from "../../components/card/index";
+import "./style.scss";
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]);
+  }
 
   return (
-    <div className='container'>
-    <h1>Lista de presença</h1>
-    <input type="text" placeholder='Digite o nome do estudante'/>
-    <button>Incluir</button>
-    <Card name="Leonardo Klein" time="10:21:40"/>
-    <Card />
-    <Card />
+    <div className="container">
+      <header>
+      <h1>Lista de presença</h1>
+      <div>
+        <strong>Leonardo Klein</strong>
+        <img src="https://github.com/LeonardoLuisKlein.png" />
+      </div>
+      </header>
+      
+
+      <input 
+      type="text" 
+      placeholder="Digite o nome do estudante"
+      onChange={(e) => setStudentName(e.target.value)} />
+      <button type="button" onClick={handleAddStudent}>Incluir</button>
+      {
+      students.map(student => (
+      <Card 
+      key={student.time}
+      name={student.name} 
+      time={student.time} 
+      />
+      ))
+      }
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
